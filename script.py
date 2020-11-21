096ffac113be289823fcacbb7d81d99883618a20
FotieMConstant <moghommahieconstant@gmail.com>
2020-11-21 13:16:58 +0100

    added the session() to post session to scrape from facebook

--- script.py
+++ script.py
@@ -1,20 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
-from secrets import username, password
 
 # payload dictionary
 
 payload = {
-    'email': username,
-    'pass': password
+    "email": "",
+    "pass": ""
 }
 
-POST_LOGIN_URL = "http://www.facebook.com/login"
+POST_LOGIN_URL = "https://mbasic.facebook.com/login"
 
 with requests.Session() as session:
     post = session.post(POST_LOGIN_URL, data=payload)
-    page = requests.get(
-        'https://mbasic.facebook.com/ufi/reaction/profile/browser/fetch/?limit=30&shown_ids=100007709478335%2C100010278907057%2C100002193171366%2C100003769000558%2C100011743639412%2C100009586518155%2C100025592177065%2C100007730594411%2C100010840262427%2C100013903305321&total_count=27&ft_ent_identifier=2793029217630700')
+    page = session.get("https://mbasic.facebook.com/ufi/reaction/profile/browser/fetch/?limit=30&shown_ids=100007709478335%2C100010278907057%2C100002193171366%2C100003769000558%2C100011743639412%2C100009586518155%2C100025592177065%2C100007730594411%2C100010840262427%2C100013903305321&total_count=27&ft_ent_identifier=2793029217630700")
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
